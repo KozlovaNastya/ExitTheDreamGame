07a3fce0df923db6b84b96c000bb335adb227a2a
KozlovaNastya <nastya.kozlic.196@gmail.com>
2025-07-08 14:18:02 +0300

    fix

--- Game/gam/levels/levels.py
+++ Game/gam/levels/levels.py
@@ -58,7 +58,7 @@ class LevelFour(BaseLevel):
     def __init__(self, parent=None):
         super().__init__(
             background_path="assets/background/level4.png",
-            platforms_data=[  # статичные платформы
+            platforms_data=[
                 (20, 500, 120, 40, "assets/for game/platform.png"),
                 (300, 360, 100, 40, "assets/for game/platform.png"),
             ],
@@ -67,7 +67,6 @@ class LevelFour(BaseLevel):
             parent=parent
         )
 
-        # Добавляем движущуюся платформу отдельно
         moving_platform1 = MovingPlatform(
             x=160, y=430, width=100, height=40,
             image_path="assets/for game/platform.png",
@@ -88,8 +87,6 @@ class LevelFour(BaseLevel):
         self.platforms.append(moving_platform2)
         moving_platform2.show()
 
-
-        # Обновляем платформы у игрока, чтобы он знал и о движущейся
         self.player.set_platforms(self.platforms)
 
 
